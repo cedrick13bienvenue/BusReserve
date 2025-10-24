a34c9a187c7d2241fb5a19792a8326ae57a72e80
bienvenue cedrick <cedrick4smith@gmail.com>
2025-10-24 23:14:00 +0200

    feat: add types barrel export file

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+// Export all types from a single entry point
+export * from './auth.types';
+export * from './booking.types';
+export * from './route.types';
+export * from './schedule.types';
+export * from './common.types';
