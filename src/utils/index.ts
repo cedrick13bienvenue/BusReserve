b134e650dd19334fff4197dd329f0011fda5be66
bienvenue cedrick <cedrick4smith@gmail.com>
2025-10-27 12:05:51 +0200

    feat: add utils barrel export file

---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+// Export all utilities from a single entry point
+export * from './dateUtils';
+export * from './validationUtils';
+export * from './responseUtils';
+export * from './generatorUtils';
+export * from './formatterUtils';
+export * from './errorUtils';
+export * from './loggerUtils';
